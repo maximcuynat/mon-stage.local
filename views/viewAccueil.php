103a6741d481b270bb493da9d9e88aaefd5edf5d
Maxim <maxim.cuynat@viacesi.fr>
2024-03-14 10:37:26 +0100

    Update viewAccueil.php

--- views/viewAccueil.php
+++ views/viewAccueil.php
@@ -3,29 +3,18 @@
 
 <div class="container px-0 text-white mt-3">
     <div class="row">
-        <div class="col-12">
-            <h1 >Bienvenue sur le site de gestion de stages</h1>
-        </div>
+        <h1 >Bienvenue sur le site de gestion de stages</h1>
+        <p >Ce site vous permet de gérer les stages, les entreprises et les contacts.</p>
+        <p >Vous pouvez ajouter, modifier, supprimer et voir les stages, les entreprises et les contacts.</p>
+        <p >Pour commencer, cliquez sur le bouton "Ajouter" dans la barre de navigation.</p>
+        <p >Bonne visite !</p>
     </div>
-    <div class="row">
-        <div class="col-12">
-            <p >Ce site vous permet de gérer les stages, les entreprises et les contacts.</p>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-12">
-            <p >Vous pouvez ajouter, modifier, supprimer et voir les stages, les entreprises et les contacts.</p>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-12">
-            <p >Pour commencer, cliquez sur le bouton "Ajouter" dans la barre de navigation.</p>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-12">
-            <p >Bonne visite !</p>
-        </div>
-    </div>
+    <!-- Ajouter mon premier stage -->
+    <a type="button" class="btn btn-primary mb-3" href="/stages/add">Ajouter mon premier stage</a><br>
 
+    <!-- Ajouter mon premier contact -->
+    <a type="button" class="btn btn-primary mb-3" href="/contacts/add">Ajouter mon premier contact</a><br>
+
+    <!-- Ajouter mon entreprise -->
+    <a type="button" class="btn btn-primary mb-3" href="/entreprises/add">Ajouter ma première entreprise</a><br>
 </div>
